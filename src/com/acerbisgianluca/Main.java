72a48bf499ac1c63d18e33b1e31f10f116318f41
Gianluca Acerbis <acerbisgianluca@gmail.com>
2019-01-20 22:59:35 +0100

    Added comments and mode selection.

--- src/com/acerbisgianluca/Main.java
+++ src/com/acerbisgianluca/Main.java
@@ -10,8 +10,13 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		tasks = new ArrayList<>();
 
+		int mode = 1;
 		try {
-			// Ask for number of tasks
+			// Modalità
+			System.out.print("Modalità: 1 (default) - Early Start/Early Finish\t2- Late Start/Late Finish (digita 1 o 2): ");
+			mode = scan.nextInt();
+
+			// Numero di attività
 			System.out.print("Quante attività ci sono (numero intero)? ");
 			int nTasks = scan.nextInt();
 			scan.nextLine();
@@ -20,18 +25,22 @@ public class Main {
 			String[] dependencies, date;
 			String name;
 			for (i = 0; i < nTasks; i++) {
+				// Nome attività
 				System.out.print("Nome della attività " + (i + 1) + ": ");
 				name = scan.nextLine();
 
+				// Data di inizio dell'attività
 				System.out.print("- Data di inizio della attività " + (i + 1) + " nel seguente formato gg/mm/aaaa (premi invio per la data di oggi): ");
 				String dateS = scan.nextLine();
 				date = dateS.trim().replaceAll(" ", "").split("/");
 				GregorianCalendar gc = dateS.equals("") ? new GregorianCalendar() : new GregorianCalendar(Integer.parseInt(date[2]), Integer.parseInt(date[1]), Integer.parseInt(date[0]));
 
+				// Durata dell'attività
 				System.out.print("- Durata della attività " + (i + 1) + " (in giorni): ");
 				duration = scan.nextInt();
 				scan.nextLine();
 
+				// Lista di precedenze dell'attività
 				System.out.print("- Dipendenze della attività " + (i + 1) + " separate dalla virgola: ");
 				dependencies = scan.nextLine().trim().replaceAll(" ", "").split(",");
 
@@ -44,9 +53,15 @@ public class Main {
 
 		int maxDur = 0, dur;
 		try {
-			for (Task t : tasks)
-				if ((dur = totalDurationLate(t, t)) > maxDur)
-					maxDur = dur;
+			// Mode 1 è quella di default e viene chiamata anche se viene inserito un numero diverso sia da 1 che da 2!
+			if (mode == 2)
+				for (Task t2 : tasks)
+					if ((dur = totalDurationLate(t2, t2)) > maxDur)
+						maxDur = dur;
+					else
+						for (Task t1 : tasks)
+							if ((dur = totalDuration(t1)) > maxDur)
+								maxDur = dur;
 		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
 		}
@@ -84,10 +99,8 @@ public class Main {
 		for(String taskName : t.getDependencies()){
 			tDep = Main.getTaskByName(taskName);
 			if((dur = totalDurationLate(tDep, t)) > maxDuration) {
-				if(t.getEnd().getTime().after(parent.getStart().getTime())){
+				if (t.getEnd().getTime().after(parent.getStart().getTime()))
 					t.setLateFinish(parent.getStart());
-				}
-
 				maxDuration = dur;
 			}
 		}
